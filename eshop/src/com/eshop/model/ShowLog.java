a98e29b233d9ef8e5bd2b9a79a57b331e06f79de
Artem Pestov <agp22888@gmail.com>
2015-12-21 19:25:34 +0300

    formatting

--- eshop/src/com/eshop/model/ShowLog.java
+++ eshop/src/com/eshop/model/ShowLog.java
@@ -6,42 +6,40 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-
 public class ShowLog {
 	private JdbcTemplate jdbcTemplate;
-	
-	public ShowLog(JdbcTemplate jdbcTemplate){
+
+	public ShowLog(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	
-	public ArrayList<LogRecord> getLog(String tableName) throws SQLException{
+
+	public ArrayList<LogRecord> getLog(String tableName) throws SQLException {
 		String query = "SELECT * FROM " + tableName;
 		ArrayList<LogRecord> list = new ArrayList<>();
 		Statement statement = null;
 		ResultSet resultSet = null;
-		try{
-			 statement = jdbcTemplate.getDataSource().getConnection().createStatement();
-			 resultSet = statement.executeQuery(query);
-			 while(resultSet.next()){
-				 list.add(new LogRecord(resultSet.getString("id"),resultSet.getString("dt"),
-						 resultSet.getString("table_name"),resultSet.getString("operation"),
-						 resultSet.getString("result")));
-			 }
-		}catch(Exception e){
+		try {
+			statement = jdbcTemplate.getDataSource().getConnection().createStatement();
+			resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				list.add(new LogRecord(resultSet.getString("id"), resultSet.getString("dt"),
+						resultSet.getString("table_name"), resultSet.getString("operation"),
+						resultSet.getString("result")));
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
-			try{
-				if(statement != null){
+		} finally {
+			try {
+				if (statement != null) {
 					statement.close();
 				}
-				if(resultSet != null){
+				if (resultSet != null) {
 					resultSet.close();
 				}
-			}catch(SQLException e){
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return list;
-		
 	}
 }
